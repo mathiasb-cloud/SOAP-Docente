e56e01928e339bc7822627b95ac57ed9ef285a39
Mathias Benites <i202406644@cibertec.edu.pe>
2026-02-28 14:32:17 -0500

    fix

--- src/main/java/cibertec/pe/feignclient/CursoFeignClient.java
+++ src/main/java/cibertec/pe/feignclient/CursoFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import cibertec.pe.entity.Curso;
 
-@FeignClient(name ="REST-CURSO", url= "${feign.cuenta.url}")
+@FeignClient(name ="REST-CURSO", url= "${feign.curso.url}")
 public interface CursoFeignClient {
 	
 	@PostMapping("/api/curso/createCurso")
