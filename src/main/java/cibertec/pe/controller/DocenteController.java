67f6f26a59f709b9fe935d13b89183039ada48a0
Mathias Benites <benitesmathiaa@gmail.com>
2026-09-09 23:59:25 -0500

    Fix typo in deleteDocente response message

--- src/main/java/cibertec/pe/controller/DocenteController.java
+++ src/main/java/cibertec/pe/controller/DocenteController.java
@@ -46,6 +46,6 @@ public class DocenteController {
 	@DeleteMapping("/deleteDocente/{codigo}")
 	public String deleteDocente(@PathVariable int codigo) {
 		docenteService.eliminarDocente(codigo);
-		return "Docente eliminado corre
+		return "Docente eliminado corr
 	
 
